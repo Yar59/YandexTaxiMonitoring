8e60ce854163d05eed933479232e2c1c6725f8bc
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-06-30 17:32:01 +0300

    add bot

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import logging
+from enum import Enum, auto
+from textwrap import dedent
+
 import httpx
 from environs import Env
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Application, ConversationHandler, CommandHandler, MessageHandler, filters, ContextTypes
+
+logger = logging.getLogger(__name__)
 
 
-def fetch_coordinates(apikey, address):
+class States(Enum):
+    start = auto()
+    handle_menu = auto()
+
+
+def fetch_coordinates(apikey: str, address: str) -> tuple[float, float] | None:
     base_url = "https://geocode-maps.yandex.ru/1.x"
     response = httpx.get(base_url, params={
         "geocode": address,
@@ -20,7 +33,8 @@ def fetch_coordinates(apikey, address):
     return lon, lat
 
 
-def get_taxi(client_id, api_key, start_coordinates, end_coordinates):
+def get_taxi(client_id: str | int, api_key: str, start_coordinates: tuple[float, float],
+             end_coordinates: tuple[float, float]) -> dict:
     url = 'https://taxi-routeinfo.taxi.yandex.net/taxi_info'
     payload = {
         'clid': client_id,
@@ -33,13 +47,63 @@ def get_taxi(client_id, api_key, start_coordinates, end_coordinates):
     return response.json()
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> States:
+    reply_keyboard = [
+        ["Выбрать маршрут", ],
+    ]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        dedent("""
+            Этот бот поможет тебе отслеживать цену на такси
+            и, если ты готов подождать, сообщит, когда цена снизится.
+        """),
+        reply_markup=markup,
+    )
+
+    return States.handle_menu
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
+    await update.message.reply_text(
+        "Надеюсь, был тебе полезен, пока!",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return ConversationHandler.END
+
+
 def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
     env = Env()
     env.read_env()
 
-    geocoder_api_key = env("GEOCODER_API_KEY")
+    geocoder_api_key = env('GEOCODER_API_KEY')
     taxi_client_id = env('TAXI_CLIENT_ID')
     taxi_api_key = env('TAXI_API_KEY')
+    tg_token = env('TG_TOKEN')
+
+    application = Application.builder().token(tg_token).build()
+
+    application.bot_data['GEOCODER_API_KEY'] = geocoder_api_key
+    application.bot_data['TAXI_CLIENT_ID'] = taxi_client_id
+    application.bot_data['TAXI_API_KEY'] = taxi_api_key
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+        },
+        fallbacks=[MessageHandler(filters.Regex("^Done$"), cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == '__main__':
